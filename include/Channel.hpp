3f1bb3b798d553466a788fed4fb383d489fd91de
David Lopez Lopez <dlopez-l@c2r12s5.42madrid.com>
2025-11-19 17:06:58 +0100

    start channel

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -1,13 +1,43 @@
-#ifndef Channel_HPP
-#define Channel_HPP
+#pragma once
 
+#include <poll.h>
+#include <unistd.h>
+#include <string>
+#include <algorithm>
+#include <deque>
 class Channel 
 {
+private:
+	std::string _channel_name;
+	std::string _channel_pass;
+	std::string _channel_topic;
+	std::deque<std::string> _members;
+	std::deque<std::string> _operators;
+	size_t _channel_limit;
+	bool _invite_mode;
+
 public:
-    Channel();
-    ~Channel();
+    Channel(const std::string &name, const std::string &pass);
     Channel(const Channel &other);
+    ~Channel();
     Channel &operator=(const Channel &other);
+
+	std::string getName();
+	std::string getPass();
+	std::string getTopic();
+	size_t getLimit();
+	bool getIniviteMode();
+
+	void setPass(std::string);
+	void setTopic(std::string);
+	void setLimit(size_t);
+	void setIniviteMode(bool);
+
+	void addMember(std::string);
+	void addOperator(std::string);
+
+	void kickMember(std::string);
+	void kickOperator(std::string);
+
 };
 
-#endif // Channel_HPP
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,6 +1,7 @@
 #include "Channel.hpp"
 
-Channel::Channel() 
+
+Channel::Channel(const std::string &name, const std::string &pass) : _channel_name(name), _channel_pass(pass)
 {
 }
 
@@ -8,7 +9,8 @@ Channel::~Channel()
 {
 }
 
-Channel::Channel(const Channel &other) 
+
+Channel::Channel(const Channel &other)
 {
     (void)other;
 }
@@ -18,4 +20,75 @@ Channel &Channel::operator=(const Channel &other)
     if (this != &other) {
     }
     return *this;
+}
+
+std::string Channel::getName()
+{
+	return std::string();
+}
+
+std::string Channel::getPass()
+{
+	return std::string();
+}
+
+std::string Channel::getTopic()
+{
+	return std::string();
+}
+
+size_t Channel::getLimit()
+{
+	return _channel_limit;
+}
+
+bool Channel::getIniviteMode()
+{
+	return _invite_mode;
+}
+
+void Channel::setPass(std::string pass)
+{
+	_channel_pass = pass;
+}
+
+void Channel::setTopic(std::string topic)
+{
+	_channel_topic = topic;
+}
+
+void Channel::setLimit(size_t l)
+{
+	_channel_limit = l;
+}
+
+void Channel::setIniviteMode(bool mode)
+{
+	_invite_mode = mode;
+}
+
+void Channel::addMember(std::string member)
+{
+	_members.push_back(member);
+}
+
+void Channel::kickMember(std::string member)
+{
+	std::deque<std::string>::iterator it;
+	it = std::find(_members.begin(), _members.end(), member);
+	if (it != _members.end())
+		_members.erase(it);
+}
+
+void Channel::addOperator(std::string member)
+{
+	_operators.push_back(member);
+}
+
+void Channel::kickOperator(std::string member)
+{
+	std::deque<std::string>::iterator it;
+	it = std::find(_operators.begin(), _operators.end(), member);
+	if (it != _operators.end())
+		_operators.erase(it);
 }
